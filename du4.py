ada7f9b8e2a980f5d0a8ca06ad8715e7d6999cdc
Anna Kožíšková <koziskoa@natur.cuni.cz>
2022-01-04 23:48:45 +0100

    výjimka pro vstupní soubor

--- du4.py
+++ du4.py
@@ -3,7 +3,7 @@ from functions import is_number, open_load, is_number
 
 parser = argparse.ArgumentParser(exit_on_error=False)
 
-parser.add_argument("-f", "--inputfile", help = "Název vstupního souboru", type = str)
+parser.add_argument("-f", "--inputfile", help = "Název vstupního souboru", type = open_load)
 parser.add_argument("-l", "--maxlength", help = "Maximální délka", type = is_number)
 parser.add_argument("-o", "--outputfile", help = "Název výstupního souboru")
 
@@ -12,7 +12,8 @@ arguments = parser.parse_args()
 print(f"Maximální délka je {arguments.maxlength}")
 print(f"Výstupní soubor je {arguments.outputfile}")'''
 
-cyklotrasy = open_load(f"{arguments.inputfile}")
+#cyklotrasy = open_load(f"{arguments.inputfile}")
+cyklotrasy = arguments.inputfile
 print(cyklotrasy)
 
 
