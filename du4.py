621377e9b1d5a1752a6ba8648ff900d8bbefb939
Anna Kožíšková <koziskoa@natur.cuni.cz>
2022-01-05 22:24:54 +0100

    kontrola souřadnic, transformace pokud WGS-84

--- du4.py
+++ du4.py
@@ -1,19 +1,32 @@
 import argparse
-from functions import is_number, open_load, is_number
+from functions import  open_load, is_positive_number, transfer_coor
 
-parser = argparse.ArgumentParser(exit_on_error=False)
+parser = argparse.ArgumentParser()
 
 parser.add_argument("-f", "--inputfile", help = "Název vstupního souboru", type = open_load)
-parser.add_argument("-l", "--maxlength", help = "Maximální délka", type = is_number)
+parser.add_argument("-l", "--maxlength", help = "Maximální délka", type = is_positive_number)
 parser.add_argument("-o", "--outputfile", help = "Název výstupního souboru")
 
 arguments = parser.parse_args()
 '''print(f"Vstupní soubor je {arguments.inputfile}")
 print(f"Maximální délka je {arguments.maxlength}")
 print(f"Výstupní soubor je {arguments.outputfile}")'''
-
-#cyklotrasy = open_load(f"{arguments.inputfile}")
 cyklotrasy = arguments.inputfile
+
+#pokud jsou souřadnice WGS-84 -> transformace souřadnic
+for i in cyklotrasy["features"]:
+    # x-ová souřadnice WGS-84 bude vždy menší než y-ová
+    # v systému S-JTSK je to přesně naopak - tedy x>y
+    if i["geometry"]["coordinates"][0][0]< i["geometry"]["coordinates"][0][1]:
+        coor=[]
+        if isinstance(i["geometry"]["coordinates"],list):
+            for j in i["geometry"]["coordinates"]:
+                j=transfer_coor(j[0],j[1])
+                coor.append(j)
+    else:
+        break
+    i["geometry"]["coordinates"]= coor
 print(cyklotrasy)
+
 
 
--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 import json
-import argparse
 from json.decoder import JSONDecodeError
+from pyproj import Transformer
+
+wgs2jtsk = Transformer.from_crs(4326,5514, always_xy=True)
+
 def open_load (name):
     """# Otevírá soubor typu json 
     - do parametru se zadá název souboru včetně uvozovek např. "soubor.json"
@@ -8,7 +11,8 @@ def open_load (name):
     try:
         with open(name, encoding="utf-8") as geojsonfile:
             reader = json.load(geojsonfile)
-        return reader
+            reader_n = v_to_number(reader)
+        return reader_n
     
     except FileNotFoundError:
         print(f"Soubor {name} se nepodařilo najít.")
@@ -20,10 +24,34 @@ def open_load (name):
         print(f"Soubor {name} je prázdný.")
         exit()
 
-def is_number (n):
+def v_to_number (geo_file):
+   """komentář"""
+   for i in geo_file["features"]:
+      if isinstance(i["geometry"]["coordinates"],list):
+         try:
+            for j in i["geometry"]["coordinates"]:
+               j[0] = float(j[0])
+               j[1] = float(j[1])
+         except IndexError:
+            print("Na pozici souřadnic x a y se nenachází číselná hodnota, nebo souřadnice chybí, nelze dále pracovat. ")
+            exit()
+      return (geo_file)
+
+def transfer_coor (x,y):
+    """## Převod souřadnicového systému z WGS84 do JTSK
+    - vrátí dvojici čísel x a y v souřadnicovém systému JTSK"""
+    return wgs2jtsk.transform(x,y)
+
+def is_positive_number (n):
+   """Pokusí se převést na číslo, pokud proběhne, zkontroluje, zda je číslo kladné"""
    try:
-      number = int(n)
+      number = float(n)
    except ValueError:
       print("Musí být zadané číslo")
       exit()
-   
+   if number < 0:
+      print("Číslo nesmí být záporné")
+      exit()
+
+cy=open_load("cyklo_WGS.geojson")
+print(cy)
