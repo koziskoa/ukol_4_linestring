496ef221ce088152d8b52f8aad0a5e3e68bae4c3
Anna Kožíšková <koziskoa@natur.cuni.cz>
2022-02-07 18:28:39 +0100

    třídy: Linestring, Segment, Point

--- geometry.py
+++ geometry.py
@@ -1,7 +1,9 @@
 from functions import distance
 
 class LineString():
-    """Vlastní obejkty linestring (-> soubor linií)"""
+    """Vlastní obejkty linestring (-> soubor linií)
+    do proměnné my_list se načte celé pole souřadnic
+    do proměnné semgments se ukládají souřadnice linií (Segment) tvořené jednotlivými body (Point)"""
     def __init__(self, l_string) -> None:
         self.my_list = l_string
         self.segments = []
@@ -9,14 +11,14 @@ class LineString():
             self.segments.append(Segment(Point(l_string[i][0], l_string[i][1]), Point(l_string[i + 1][0], l_string[i + 1][1])))
         
     def divide_long_segments (self, max_lenght):
+        """prochází pole liní (segments) a uplatňuje na každý segment metodu divide(max_lenght)"""
         new_segments_list = []
         for segment in self.segments:
-            #semi_result = segment.divide(max_lenght)
-            #print(f"{semi_result.p1.xy} + {semi_result.p2.xy}")
             new_segments_list.extend(segment.divide(max_lenght))
         self.my_list = new_segments_list
     
     def segment_to_point_list (self):
+        """Tato metoda prochází pole linií (my_list) a vrací nové pole bodů"""
         final_list = []
         for segment in self.my_list:
             final_list.append(segment.p1.xy)
@@ -24,17 +26,21 @@ class LineString():
         return(final_list)
 
 class Segment():
-    """Vlastní obejkty čar"""
+    """Vlastní obejkty linií, proměnné p1 a p2 jsou body, které tvoří jednu linii"""
     def __init__(self, p1, p2) -> None:
         self.p1 = p1
         self.p2 = p2
     def divide(self, max_lenght):
+        """Kontroluje, zdali je délka segmentu kratší než stanová vzdálenost (max_lenght).
+            - pokud je vzdálenost segmentu kratší než stanovená vzdálenost: - vrátí stejný segment zpět.
+            - pokud je vzdálenost segmentu větší než stanovená vzdálenost: - vrátí 2 segmenty s vytvořeným novým bodem"""
         if distance(self.p1.xy,self.p2.xy) < max_lenght:
             return [self]
 
         else: 
-            x=abs((self.p1.xy[0]-self.p2.xy[0])/2)
-            y=abs((self.p1.xy[1]-self.p2.xy[1])/2)
+            x=abs((self.p1.xy[0]-self.p2.xy[0])/2) # mezivýpočet pro souřadnici x
+            y=abs((self.p1.xy[1]-self.p2.xy[1])/2) # mezivýpočet pro souřadnici y
+            # 4 možnosti správného určení nového bodu
             if self.p1.xy[0]<self.p2.xy[0]:
                 if self.p1.xy[1]<self.p2.xy[1]:
                     new_p = Point(self.p1.xy[0]+x,self.p1.xy[1]+y)
@@ -52,52 +58,6 @@ class Segment():
             return new_line1 + new_line2
 
 class Point ():
+    """Vlatní objekty bodů, proměnné x a y jsou souřadnice bodu"""
     def __init__(self, x, y) -> None:
         self.xy = [x, y]
-
-
-'''vzor = {
-	"type" : "FeatureCollection",
-	"name" : "DOP_Cyklotrasy_l",
-	"crs" : {
-		"type" : "name",
-		"properties" : {
-			"name" : "EPSG:5514"
-		}
-	},
-	"features" : [
-		{
-			"type" : "Feature",
-			"geometry" : {
-				"type" : "LineString",
-				"coordinates" : [
-					[ -729120.2305283286, -1047412.9380170368 ],
-					[ -729117.334126506, -1047426.4983255751 ],
-					[ -729114.9620250128, -1047445.0871372782 ],
-					[ -729113.5667241365, -1047465.0529498458 ],
-					[ -729103.7995179854, -1047550.0110033341 ],
-					[ -729100.86941614, -1047574.6584188528 ],
-					[ -729095.1458125375, -1047612.6914427951 ],
-					[ -729092.5928109288, -1047647.5884647667 ]
-				]
-			},
-			"properties" : {
-				"OBJECTID" : 1,
-				"CISLO_TRAS" : "A440",
-				"CT" : 0,
-				"REALIZACE" : 1,
-				"JEDNOSMERKA" : 0,
-				"DOPR_STAV" : 123,
-				"Shape_Length" : 236.41044043711238
-			}
-		}]}
-geo_s = []
-geo_l = []
-geo_p = []
-for i in vzor["features"]:
-    if isinstance(i["geometry"]["coordinates"],list):
-        geo_s.append(i["geometry"]["coordinates"])
-print(geo_s)
-geo_s=LineString(geo_s)
-geo_s.divide_long_segments(10)
-print(geo_s)'''
